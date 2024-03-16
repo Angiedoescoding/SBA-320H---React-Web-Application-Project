c2677f60b355c5f6ab46c071e1a7dc6149000346
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-16 12:46:06 -0700

    navbar & importing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
-
+import Navbar from './components/Navbar';
+import MainPage from './components/MainPage';
+import SearchFilter from './components/SearchFilter';
+import Toggle from './components/Toggle';
 
 function App() {
 
   
   return (
     <>
-
+      <Navbar />
+      <MainPage />
+      <SearchFilter />
+      <Toggle />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#about" target="_blanked" id="about">About</a></li>
+                <li><a href="#schedule" id="navItems">Schedule</a></li>
+                <li><a href="https://www.artic.edu" target="_blanked" id="navItems">Art Resources</a></li>
+                <li><a href="#login" id="navItems">Login</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
