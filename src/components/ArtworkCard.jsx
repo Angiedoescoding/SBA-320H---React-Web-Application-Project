1f9df8aca4d704a072b0635acfbe02b6c4ee6c20
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-17 21:53:57 -0700

    add the artwork to favorites for  the useState requirement+npm icon added

--- src/components/ArtworkCard.jsx
+++ src/components/ArtworkCard.jsx
@@ -1,11 +1,37 @@
+// Testing an additional requirement - to allow the user to add the artwork to their favorite to meet the useState requirement (to manage the sate of whether an artwirk is favorited by a user and to keep the choice of fav artworks across sessions and updating the UI to reflect the changes)
+
+// npm i react-icons
+
+import { useState } from 'react';
+import { FaHeart } from "react-icons/fa";
+
 const ArtworkCard = ({ artwork }) => {
+
+    // State to track whether artwork is favorited
+    const [Favorite, setFavorite] = useState(false);
+    
+    const toggleFavorite = () => {
+        setFavorite(!Favorite);
+
+        if (Favorite) {
+            console.log(`Removed the "${artwork.title}" from favorites.`);
+        } else {
+            console.log(`Added the "${artwork.title} to favorites."`)
+        }
+    };
+
     return (
     <div className="artwork-card">
         <h3  className="h3Dark">{artwork.title}</h3>
+        <img src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/100,/0/default.jpg`} alt={artwork.title} />
         <p>Artist: {artwork.artist_title}</p>
             <p>Date: {artwork.date_display}</p>
             <p>Details: {artwork.place_of_origin}</p>
             <p>{artwork.description}</p>
+
+            <button onClick={toggleFavorite}>
+                <FaHeart color={Favorite ? 'red' : 'gray'} />
+            </button>
     </div>
     )
 }
