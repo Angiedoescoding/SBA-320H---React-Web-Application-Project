b0d47b41680c88d56ce5bad3e633b77a6e53b0b1
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-18 13:00:54 -0700

    testig Fav List props/array

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -1,3 +1,20 @@
 // Need to pass the favorites list as props to this component to display the LIST of FAVS to the user
     // When is favorited, add it to the favorites list; when unfavorited - remove from the fav list
     // Accessing the list through a button/link
+
+    const FavoritesList = ({ favorites }) => {
+        const favsToDisplay = favorites || [];      // checking if favorites is undefined (console Uncaught TypeError error) or null and provide any fallback value to show/use
+
+        return (
+            <div className="favs-list">
+                <h2>My Favorite Art List</h2>
+                <ul>
+                {Array.isArray(favsToDisplay) && favsToDisplay.map((favorite, index) => (             // map through the favorites array  and render each favorite artwork as a list item
+                        <li key={index}>{favorite.title}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+    export default FavoritesList;
